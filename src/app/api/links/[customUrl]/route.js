39e82bded2f67f8bc5e92ba8d8996e6d4812a686
juan <juanbejamingiupponi@gmail.com>
2024-10-29 12:14:06 -0300

    geolocation logs fixed

--- src/app/api/links/[customUrl]/route.js
+++ src/app/api/links/[customUrl]/route.js
@@ -1,4 +1,5 @@
 import { prisma } from "@/lib/prisma";
+import { handleError } from "@/utils/handleError";
 import { NextResponse } from "next/server";
 
 export async function GET(req, { params }) {
@@ -6,69 +7,71 @@ export async function GET(req, { params }) {
     const headers = req.headers;
     const ip = headers.get("x-forwarded-for") || headers.get("x-real-ip") || 'Unknown';
 
-    const link = await prisma.link.findUnique({
-        where: {
-            customUrl: customUrl,
-        },
-        select: {
-            url: true,
-            id: true,
-        },
-    });
+    try {
+        const link = await prisma.link.findUnique({
+            where: {
+                customUrl: customUrl,
+            },
+            select: {
+                url: true,
+                id: true,
+            },
+        });
 
-    if (!link) {
+        if (!link) {
+            return NextResponse.json({
+                error: "Link not found",
+            }, {
+                status: 404,
+            });
+        }
+
+        const geolocationApiRes = await fetch(`http://ip-api.com/json/${ip}?fields=16607`);
+
+        const geolocationApiResJson = await geolocationApiRes.json();
+
+        console.log({
+            geolocationApiResJson
+        });
+
+        if (geolocationApiResJson.status === 'fail') {
+            const click = await prisma.click.create({
+                data: {
+                    linkId: link.id,
+                    ip: ip,
+                },
+            });
+
+            console.log({
+                type: 'No geolocation data',
+                click
+            });
+
+        } else {
+            const click = await prisma.click.create({
+                data: {
+                    linkId: link.id,
+                    city: geolocationApiResJson.city,
+                    country: geolocationApiResJson.country,
+                    region: geolocationApiResJson.regionName,
+                    regionCode: geolocationApiResJson.region,
+                    countryCode: geolocationApiResJson.countryCode,
+                    latitude: geolocationApiResJson.lat,
+                    longitude: geolocationApiResJson.lon,
+                    ip: ip,
+                },
+            });
+
+            console.log({
+                type: 'With geolocation data',
+                click
+            });
+        }
+
         return NextResponse.json({
-            error: "Link not found",
-        }, {
-            status: 404,
+            url: link.url,
         });
+    } catch (error) {
+        handleError(error);
     }
-
-    const geolocationApiRes = await fetch(`http://ip-api.com/json/${ip}?fields=16607`);
-
-    const geolocationApiResJson = await geolocationApiRes.json();
-
-    console.log({
-        geolocationApiResJson
-    });
-
-    if (geolocationApiResJson.status === 'fail') {
-        const click = await prisma.click.create({
-            data: {
-                linkId: link.id,
-                ip: ip,
-            },
-        });
-
-        console.log({
-            type: 'No geolocation data',
-            click
-        });
-        
-    } else {
-        const geolocationData = await geolocationApiRes.json();
-
-        const click = await prisma.click.create({
-            data: {
-                linkId: link.id,
-                city: geolocationData.city,
-                country: geolocationData.country,
-                region: geolocationData.regionName,
-                regionCode: geolocationData.region,
-                countryCode: geolocationData.countryCode,
-                latitude: geolocationData.lat,
-                longitude: geolocationData.lon,
-                ip: ip,
-            },
-        });
-
-        console.log({
-            type: 'With geolocation data',
-            click
-        });
-    }
-
-    return NextResponse.json({
-        url: link.url,
-    });
 }
